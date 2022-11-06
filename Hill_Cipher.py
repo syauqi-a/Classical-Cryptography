3e0162adef896f115e8da5a66e166cd991f26d80
Syauqi <kahmadsyauqi@upi.edu>
2022-11-06 16:54:51 +0700

    Manual way to find inverse matrix instead of Numpy

--- Hill_Cipher.py
+++ Hill_Cipher.py
@@ -13,6 +13,34 @@ class Hill_Chiper:
         key.resize((3, 3), refcheck=False)
         return key
 
+    def findDeterminant(self, key_arr):
+        k = np.copy(key_arr)
+        col1 = k[0][0]*((k[1][1]*k[2][2]) - (k[2][1]*k[1][2]))
+        col2 = k[0][1]*((k[1][0]*k[2][2]) - (k[2][0]*k[1][2]))
+        col3 = k[0][2]*((k[1][0]*k[2][1]) - (k[2][0]*k[1][1]))
+        return col1 - col2 + col3
+
+    def findAdjoint(self, key_arr):
+        k = np.copy(key_arr)
+        adj_key = np.eye(3, dtype=int)
+        for i in range(3):
+            for j in range(3):
+                x = (j+1)%3
+                y = (i+1)%3
+
+                a = k[x][y] * k[(x+1)%3][(y+1)%3]
+                b = k[(x+1)%3][y] * k[x][(y+1)%3]
+                adj_key[i][j] = (a - b) % 26
+        return adj_key
+
+    def findInvMod(self, numb, p):
+        i = 1
+        r = (i*numb) % p  # get reminder
+        while r != 1:
+            i += 1
+            r = (i*numb) % p  # get reminder
+        return i
+
     def encrypt(self, text:str, key:str|None = None):
         # Remove non-alphabetic character and convert text into uppercase
         text = re.sub(r"[^A-Z]", "", text.upper())
@@ -39,9 +67,9 @@ class Hill_Chiper:
     def decrypt(self, text:str, key:str|None = None):
         key = self.generateKeyArray(key) if key else self.key
         # Find inverse matrix of key
-        det_key = round(np.linalg.det(key))
-        adj_key = np.array(np.around(np.linalg.pinv(key)*det_key), dtype=int)
-        inv_key = (pow(det_key%26, -1, 26) * adj_key%26) % 26
+        det_key = self.findDeterminant(key)
+        adj_key = self.findAdjoint(key)
+        inv_key = (self.findInvMod(det_key, 26) * adj_key) % 26
 
         # Decrypting text by call encrypt function
         # But the key is inverse of original key
